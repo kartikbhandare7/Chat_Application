29a2602536c05d490261753ba9fea9216315213d
kartikbhandare7 <kartikbhandare57@gmail.com>
2026-07-12 20:32:17 +0530

    Change socket connection URL to production

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://chat-application-2-zaro.onrender.com", {
     autoConnect: true,
     transports: ["websocket"],
 });
