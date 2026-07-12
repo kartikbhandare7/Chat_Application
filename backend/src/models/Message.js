44959a7616661cfa5246eff583a947159148ceea
Kartik Bhandare <kartikbhandare57@gmail.com>
2026-07-12 10:25:17 +0530

    created messageSchema

---
+++ backend/src/models/Message.js
@@ -0,0 +1,22 @@
+const mongoose  = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+    {
+        username:{
+            type : String,
+            required : true
+        },
+        text:{
+            type : String,
+            required : true
+        },
+        timestamp:{
+            type : Date,
+            default : Date.now
+        }
+    },
+    {
+        timestamps : true
+    }
+);
+module.exports = mongoose.model("Message", messageSchema);   
